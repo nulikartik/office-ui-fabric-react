e6fc43f599a84efba860801e79a2b9d24be24195
Kartik Nuli (Zen3 Infosolutions America Inc) <v-kanuli@microsoft.com>
2018-08-01 09:20:40 -0700

    test

--- packages/dashboard/src/components/Card/examples/Card.Large.Basic.Example.tsx
+++ packages/dashboard/src/components/Card/examples/Card.Large.Basic.Example.tsx
@@ -96,7 +96,7 @@ export class LargeCardBasicExample extends React.Component<{}, {}> {
       { x: 34, y: 25 },
       { x: 40, y: 13 },
       { x: 48, y: 43 },
-      { x: 57, y: 30 },
+      { x: 57, y: 20 },
       { x: 64, y: 45 },
       { x: 72, y: 12 },
       { x: 78, y: 50 },
